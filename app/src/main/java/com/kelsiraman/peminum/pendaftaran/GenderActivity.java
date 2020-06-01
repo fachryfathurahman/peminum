799649ff4fffefc304c9731f870ec1b1e4638b90
fachryfathurahman <fachry123fathurahman@gmail.com>
2020-06-01 14:24:44 +0700

    gender layout

---
+++ app/src/main/java/com/kelsiraman/peminum/pendaftaran/GenderActivity.java
@@ -0,0 +1,16 @@
+package com.kelsiraman.peminum.pendaftaran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kelsiraman.peminum.R;
+
+public class GenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gender);
+    }
+}

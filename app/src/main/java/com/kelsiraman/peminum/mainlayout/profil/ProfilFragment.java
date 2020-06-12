155691bac9e5074acdb1dcf11019e70302ae043b
Ricky Hendrawan <ricky.hndrwn98@gmail.com>
2020-06-12 20:55:32 +0700

    Update ProfilFragment.java

--- app/src/main/java/com/kelsiraman/peminum/mainlayout/profil/ProfilFragment.java
+++ app/src/main/java/com/kelsiraman/peminum/mainlayout/profil/ProfilFragment.java
@@ -18,8 +18,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -77,6 +80,18 @@ public class ProfilFragment extends Fragment implements View.OnClickListener {
     }
 
     private void sendUpdateProfile() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.updateEmail(emailBaru)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("EmailUpdate", "User email address updated.");
+                        }
+                    }
+                });
+
         final int berat = Integer.parseInt(beratBaru);
 
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
